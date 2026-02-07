180ff530d91ca49e2e2d51be32cd15237075f1d4
Kavindi Dewmini Navodya <kavindinavodyadewmini99@gmail.com>
2026-02-07 18:45:00 +0530

    feat: connect Firebase database and store shopping list items

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-import { DB_URL } from "./config";
+import { DB_URL } from "./config.js";
+import { initializeApp, getDatabase, ref, push } from "./config.js";
 
-const appSettings = {
-    databaseURL: DB_URL
-}
+const appSettings = { databaseURL: DB_URL };
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
 
 const inputField = document.getElementById('input-field');
 const addButton = document.getElementById('add-button');
 
-addButton.addEventListener("click", function(){
+addButton.addEventListener("click", function() {
     let fieldValue = inputField.value;
-    alert(fieldValue);
-})
+    
+    push(shoppingListInDB, fieldValue);
+    
+    console.log(`${fieldValue} added to database`);
+    
+    inputField.value = ""; 
+    
+});
