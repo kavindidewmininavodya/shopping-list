964ee55a184a5a7c5ee0c444e08cacf0e961a6c1
Kavindi Dewmini Navodya <kavindinavodyadewmini99@gmail.com>
2026-02-09 18:56:26 +0530

    Add config.js file. It has feat: configure Firebase Realtime Database integration
    - Added config.js with Firebase imports and DB_URL
    - Exported initializeApp, getDatabase, ref, push, onValue

---
+++ config.js
@@ -0,0 +1,13 @@
+export const DB_URL = "";
+
+import { initializeApp } from "";
+import { 
+    getDatabase, 
+    ref, 
+    push, 
+    onValue,
+    remove, 
+    update 
+} from "";
+
+export { initializeApp, getDatabase, ref, push, onValue, remove, update };
